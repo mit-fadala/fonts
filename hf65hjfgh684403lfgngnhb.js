321c65e455b41e0354deb53dcf8345487c83cae9
mit-fadala <ahmed.samy98765017@yahoo.com>
2017-09-13 02:08:13 +0200

    Create hf65hjfgh684403lfgngnhb.js

---
+++ hf65hjfgh684403lfgngnhb.js
@@ -0,0 +1,9 @@
+//<![CDATA[
+$(document)['ready'](function () {
+        $('#buy')['html']('<a href="http://elawaael.blogspot.com" rel="dofollow" target="_blank" style="visibility: visible !important;">ElawaaelBlog</a>');
+        setInterval(function () {
+                if (!$('#buy:visible')['length']) {
+                  window['location']['href'] = 'http://elawaael.blogspot.com';
+                };
+            }, 3000);
+    });
